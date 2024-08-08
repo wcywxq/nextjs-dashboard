7d276e593ff3b58a9b884915d3f482751f48927b
wcywxq <wcywxq@gmail.com>
2024-08-08 13:24:37 +0800

    feat: chapter 14

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -15,11 +15,12 @@ export async function fetchRevenue() {
     // Don't do this in production :)
 
     console.log('Fetching revenue data...');
-    await new Promise((resolve) => setTimeout(resolve, 3000));
+    // TODO
+    // await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const data = await sql<Revenue>`SELECT * FROM revenue`;
 
-    console.log('Data fetch completed after 3 seconds.');
+    // console.log('Data fetch completed after 3 seconds.');
 
     return data.rows;
   } catch (error) {
@@ -157,7 +158,7 @@ export async function fetchInvoiceById(id: string) {
       // Convert amount from cents to dollars
       amount: invoice.amount / 100,
     }));
-
+    console.log(invoice)
     return invoice[0];
   } catch (error) {
     console.error('Database Error:', error);
